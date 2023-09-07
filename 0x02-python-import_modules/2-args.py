5aaba0d52c709bdbf0a3d55e7646fdb3c7b30193
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-07 07:08:03 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if (__name__ == "__main__"):
     import sys
 
     number = len(sys.argv) - 1
-    if (number < 1):
+    if (number == 0):
         print("0 arguments")
     elif (number == 1):
         print("1 argument:")
