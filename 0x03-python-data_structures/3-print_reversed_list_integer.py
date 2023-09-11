6b24445e11c1e4614c748a9b225b0fd2623907b1
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-11 11:03:09 -0700

    print_reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if (my_list):
+        my_list.reverse()
+        length = len(my_list)
+        for i in range(length):
+            print("{:d}".format(my_list[i]))
