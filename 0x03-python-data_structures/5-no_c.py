3575e7adfc1666d4245f32bd5e6f0d87cbe4dab4
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-11 11:45:49 -0700

    no

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    n_string = my_string.replace('c', '').replace('C', '')
+    n_string = my_string.translate({ord(i): None for i in 'cC'})
     return n_string
