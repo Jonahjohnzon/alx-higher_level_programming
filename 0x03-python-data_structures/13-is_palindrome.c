1dc76c37c3e9d5dd3fc17c8994f3dd585e6f2db2
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-11 13:50:06 -0700

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+
+/**
+ * reverse_list - linked reverse
+ * @head: pointer
+ * Return: void
+ */
+void reverse_list(listint_t **head)
+{
+	listint_t *p = NULL;
+	listint_t *curr = *head;
+	listint_t *next = NULL;
+
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = pre;
+		pre = curr;
+		curr = next;
+	}
+
+	*head = pre;
+}
+
+/**
+ * is_palindrome - function
+ * @head: input
+ * Return: int
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *sl = *head, *fast = *head, *tmp = *head;
+	listint_t *dp = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return (1);
+	}
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dp = sl->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dp = sl->next->next;
+			break;
+		}
+		sl = sl->next;
+	}
+	reverse_list(&dp);
+	while (dp && tmp)
+	{
+		if (temp->n == dup->n)
+		{
+			dp = dp->next;
+			tmp = tmp->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	if (!dp)
+		return (1);
+
+	return (0);
+}
