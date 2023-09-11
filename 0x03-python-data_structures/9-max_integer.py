a2a1a1ea75ae101fafdd818c5b6deb87125fd286
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-11 13:22:41 -0700

    multiple

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,8 @@ def max_integer(my_list=[]):
     if (length == 0):
         return "None"
     else:
-        maxs = my_list[0]
-        for c in range(len(my_list)):
-            if my_list[c] > maxs:
-                maxs = my_list[c]
-        return maxs
+        max = my_list[0]
+        for c in range(length):
+            if my_list[c] > max:
+                max = my_list[c]
+        return max
