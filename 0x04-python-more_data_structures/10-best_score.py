0de8c8ff518140d886e022d4fc927c308aa8f5ec
Jonah Ominyi <jonahjohnzon@gmail.com>
2023-09-13 13:23:04 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if (a_dictionary):
+        keys = list(a_dictionary.kays())
+        highest = 0
+        top = ""
+        for key in keys:
+            if (a_dictionary[key] > highest):
+                highest += a_dictionary[key]
+                top = key
+        return top
