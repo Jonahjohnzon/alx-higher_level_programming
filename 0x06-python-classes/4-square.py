94337868ea92c2e107739fa8ac4ac3caeeb8e70d
jonah ominyi <jonahjohnzon@gmail.com>
2023-09-27 13:45:44 -0700

    setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Private instance'''
+
+
+class Square:
+    '''square size'''
+    def __init__(self, size=0):
+        '''initialization'''
+        self.__size = size
+
+    def area(self):
+        '''area'''
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        '''getters'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''setters'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
