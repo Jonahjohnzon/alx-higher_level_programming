4087e673641df9893d9a9d0a7f94fafa5e2ce10a
jonah ominyi <jonahjohnzon@gmail.com>
2023-09-27 13:49:53 -0700

    setters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,8 +20,8 @@ class Square:
     @size.setter
     def size(self, value):
         '''setters'''
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        elif (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
