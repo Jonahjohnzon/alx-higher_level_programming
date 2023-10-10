d3b42ed63adee4f0502adce1fce70d8f30996779
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-10 11:55:51 -0700

    student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,5 +14,5 @@ class Student:
         """get dictionary"""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for j in attrs if hasattr(self, j)}
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
