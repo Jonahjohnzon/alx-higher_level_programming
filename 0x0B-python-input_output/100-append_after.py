d47ba3309e524982f2aee0506c408f8541927379
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-10 15:13:28 -0700

    append

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""text file function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function"""
+
+    txt = ""
+
+    with open(filename) as f:
+        for line ine f:
+            txt += line
+            if (search_string in line):
+                txt += new_string
+    with open(filename, "w") as writ:
+        writ.write(txt)
