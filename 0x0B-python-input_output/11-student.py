0eb28f04e3bae005b4df0a77a7d969f41dae69a6
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-10 12:04:13 -0700

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -16,6 +16,7 @@ class Student:
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
     def reload_from_json(self, json):
         """Replace attribute"""
         for k, x in json.items():
