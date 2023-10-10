9ec77c85b2658b0b721263daf1fce1de0d624b81
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-10 07:10:23 -0700

    correction

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/us.r/bin/python3
 """function that reads file"""
 
 
 def write_file(filename="", text=""):
     """Write in file"""
-    with open(filename,"w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         print(f.write(text))
