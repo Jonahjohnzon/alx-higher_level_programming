b211387da6f135cb59a19bdd04eef3b3f5c6aa40
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-11 13:35:01 -0700

    kind of class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
 Class
+
 """
 
 
 def is_kind_of_class(obj, a_class):
-        """return true or false"""
+    """return true or false"""
     return (isinstance(obj, a_class))
