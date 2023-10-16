d617ea3a7133530040012e3fc9554c4f8fb704cc
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-16 06:20:21 -0700

    typeerror rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,4 +12,16 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    
+    @property
+    def width(self):
+        """ getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        if (value <= 0):
+            raise ValueError("width must be > 0")
+        self.__width = value
