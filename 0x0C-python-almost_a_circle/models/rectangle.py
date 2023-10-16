866ea0d8df4493e18cd2d86c47c7f9972961cf92
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-16 07:03:36 -0700

    #

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,11 +6,11 @@ class Rectangle(Base):
     """implement base"""
     def __init__(self, width, height, x=0, y=0, id=None)
     """initialization"""
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+    super().__init__(id)
+    self.width = width
+    self.height = height
+    self.x = x
+    self.y = y
 
     @property
     def width(self):
@@ -30,11 +30,27 @@ class Rectangle(Base):
         """Setter"""
         return self.__height
 
-
     @height.setter
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value    
+        self.__height = value
+
+    def area(self):
+        """Area of rectangle."""
+        return self.width * self.height
+
+    def display(self):
+        """
+            stdout the rectangle instance with '#'
+        """
+        rec = ""
+        ps = "#"
+
+        print("\n" * self.y, end="")
+
+        for i in range(self.height):
+            rec += (" " * self.x) + (ps*self.width) + "\n"
+        print(rec, end="")
