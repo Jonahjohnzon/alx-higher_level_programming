3788e80801a307d8107e2a67064f071d54a045ff
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-16 05:35:01 -0700

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class rectangle"""
+
+
+class Rectangle(Base):
+    """implement base"""
+    def __init__(self, width, height, x=0, y=0, id=None)
+    """initialization"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    
