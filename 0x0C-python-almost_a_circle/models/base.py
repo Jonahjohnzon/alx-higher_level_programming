3cc2c512053cad198e5462563bcffaa3a4ea3894
jonah ominyi <jonahjohnzon@gmail.com>
2023-10-16 11:50:18 -0700

    chmode

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,81 @@ class Base:
                 n = cls(1)
             n.update(**dictionary)
             return n
+
+        @classmethod
+    def load_from_file(cls):
+        """load_from_file"""
+        fil = str(cls.__name__) + ".json"
+        try:
+            with open(fil, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """save to file"""
+        filen = cls.__name__ + ".csv"
+        with open(filen, "w", newline="") as csvfile:
+            if (list_objs is None or list_objs == []):
+                csvfile.write("[]")
+            else:
+                if (cls.__name__ == "Rectangle"):
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writ = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writ.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """load from file"""
+        filen = cls.__name__ + ".csv"
+        try:
+            with open(filen, "r", newline="") as csvfile:
+                if (cls.__name__ == "Rectangle"):
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dict = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dict = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dict]
+                return [cls.create(**d) for d in list_dict]
+        except IOError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Rectangles and Squares
+        """
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+        turtle.exitonclick()
