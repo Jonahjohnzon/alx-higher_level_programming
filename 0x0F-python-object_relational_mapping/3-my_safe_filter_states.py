ce3af0f8245d32cc0a40a8e54311152831635c20
jonah ominyi <jonahjohnzon@gmail.com>
2023-12-17 11:58:00 -0800

    push

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,11 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curso = db.cursor()
-    matchs = sys.argv[4]
-    curso.execute("SELECT * FROM states WHERE name LIKE %s", (matchs, ))
-    rowz = curso.fetchall()
-    
-    for a in rowz:
+    match = sys.argv[4]
+    curso.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    row = curso.fetchall()
+    for a in row:
         print(a)
     curso.close()
     db.close()
