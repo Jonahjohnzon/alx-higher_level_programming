6dd0a8a3f13fa06e5656a52a0b163f2750aeb16b
jonah ominyi <jonahjohnzon@gmail.com>
2024-01-05 10:53:49 -0800

    push

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,22 +3,22 @@
 
 
 def find_peak(list_of_integers):
-    """Funtion"""
+    """Function"""
 
     if list_of_integers is None or list_of_integers == []:
         return None
-    l = 0
-    hi = len(list_of_integers)
-    middle = ((hi - l) // 2) + l
-    middle = int(middle)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
+
+    start, end = 0, len(list_of_integers)
+    middle = (start + end) // 2
+
+    if end == 2:
         return max(list_of_integers)
-    if list_of_integers[middle] >= list_of_integers[middle - 1] and\
+
+    if list_of_integers[middle] >= list_of_integers[middle - 1] and \
             list_of_integers[middle] >= list_of_integers[middle + 1]:
         return list_of_integers[middle]
+
     if middle > 0 and list_of_integers[middle] < list_of_integers[middle + 1]:
         return find_peak(list_of_integers[middle:])
-    if middle > 0 and list_of_integers[middle] < list_of_integers[middle - 1]:
+    elif middle > 0 and list_of_integers[middle] < list_of_integers[middle - 1]:
         return find_peak(list_of_integers[:middle])
