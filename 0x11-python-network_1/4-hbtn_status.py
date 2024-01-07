eaf469fbba550f2ac2e0fcb5c37c6b7838d844fe
jonah ominyi <jonahjohnzon@gmail.com>
2024-01-07 15:11:47 -0800

    push

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""fetch https://intranet.hbtn.io/status."""
+"""Fetch https://intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
-    result = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(result.text)))
-    print("\t- content: {}".format(result.text))
+    try:
+        result = requests.get("https://intranet.hbtn.io/status")
+        result.raise_for_status()  # Check for HTTP errors
+
+        print("Body response:")
+        print("\t- type: {}".format(type(result.text)))
+        print("\t- content: {}".format(result.text))
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""takes in URL,
-- sends request to URL and displays the value
+"""Takes in a URL,
+sends a request to the URL and displays the value of the "X-Request-Id" header.
 """
 import sys
 import urllib.request
 
 if __name__ == "__main__":
+    # Assuming a URL is always provided as a command-line argument
     link = sys.argv[1]
 
-    request = urllib.request.Request(link)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    try:
+        request = urllib.request.Request(link)
+        with urllib.request.urlopen(request) as response:
+            x_request_id = response.headers.get("X-Request-Id")
+
+            if x_request_id is not None:
+                print("X-Request-Id:", x_request_id)
+            else:
+                print("The 'X-Request-Id' header is not present in the response.")
+    except urllib.error.URLError as e:
+        print(f"Error: {e}")
